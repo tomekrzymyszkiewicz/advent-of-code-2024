b8e188fc25b7c1921150dad808a7b857ae07eb15
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2024-12-03 22:48:05 +0100

    simplifed, added assert

--- day03/task2.py
+++ day03/task2.py
@@ -1,4 +1,6 @@
 import re
+from functools import reduce
+from operator import mul
 
 with open("input.txt", "r") as f:
     program = f.read()
@@ -8,16 +10,12 @@ muls_sum = 0
 command_p = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)|do\(\)|don't\(\)")
 num_p = re.compile(r"[0-9]{1,3}")
 last_command = "do()"
-for command in re.findall(command_p, program):
-    match command:
-        case command if "mul" in command:
-            if last_command == "do()":
-                x, y = re.findall(num_p, command)
-                muls_sum += int(x) * int(y)
-            elif last_command == "don't()":
-                continue
-        case command if "do" in command:
-            last_command = command
 
+for command in re.findall(command_p, program):
+    if "mul" in command and last_command == "do()":
+        muls_sum += reduce(mul, map(int, re.findall(num_p, command)))
+    else:
+        last_command = command
 
 print(muls_sum)
+assert muls_sum == 100411201
