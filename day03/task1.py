564d2f62ff1d2aeaeb3f3b902c68e2e1d99c876a
tomekrzymyszkiewicz <tomek.rzymyszkiewicz@gmail.com>
2024-12-03 22:43:37 +0100

    added assert, and reduce

--- day03/task1.py
+++ day03/task1.py
@@ -1,13 +1,22 @@
 import re
+from functools import reduce
+from operator import mul
+
 with open("input.txt", "r") as f:
     program = f.read()
 
 pos = 0
 muls_sum = 0
-mul_p = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)")
-num_p = re.compile(r"[0-9]{1,3}")
-for mul in re.findall(mul_p,program):
-    x,y = re.findall(num_p,mul)
-    muls_sum += int(x)*int(y)
-print(muls_sum)
+mul_pattern = re.compile(r"mul\([0-9]{1,3},[0-9]{1,3}\)")
+num_pattern = re.compile(r"[0-9]{1,3}")
 
+
+muls_sum = sum(
+    [
+        reduce(mul, map(int, re.findall(num_pattern, mul_part)))
+        for mul_part in re.findall(mul_pattern, program)
+    ]
+)
+
+print(muls_sum)
+assert muls_sum == 174103751
